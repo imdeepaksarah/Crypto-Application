9b30fb67b708e43c98d999df52ab6fea4c56fedf
unknown <deepaksarahyt@gmail.com>
2024-02-28 18:07:33 +0530

    first commit

---
+++ src/Components/HeroFront.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HeroFront = () => {
+  return (
+ 
+<div className="box flex justify-center static">
+<div className="bg-transparent rounded-lg flex-wrap ">
+    <span>
+    <img className="w-10 mx-5 mt-6 "src="https://logodownload.org/wp-content/uploads/2017/06/bitcoin-logo-1-1.png" alt="" />
+      <h1 className="text-slate-500 font-semibold text-4xl font-bold ">
+       BitCoin
+      </h1>
+    </span>
+       
+      <div className="text-4xl leading-8 font-bold text-slate-600  mt-2">
+        $39.00
+      </div>
+      </div>
+
+     
+     </div>
+  )
+}
+
+export default HeroFront
